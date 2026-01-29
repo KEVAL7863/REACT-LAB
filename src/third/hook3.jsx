6075d05a6c958cfc73effe678a3e17c4afe326fc
Keval Gelani <kevalgelani7863@gmail.com>
2026-01-29 09:01:26 +0530

    modified files

---
+++ src/third/hook3.jsx
@@ -0,0 +1,19 @@
+// cretae a register from with following details
+// 1 . Name(Text)
+// 2 . Email(Text)
+// 3 . password(text, Regular Expression)
+// 4 . Confrim password(text, Regular Expression)
+//  5. Gender(Radio Button)
+//  6. country(Dropdown)
+//  7. Term and conditions(checkbox)
+//  8. color picker(color)
+//  9. DOB(Date picker)
+//  10. submit Button
+
+
+
+import React, {Component} from "react";
+
+class StateExercise extends Component {
+    
+}
--- src/third/usestate3.jsx
+++ src/third/usestate3.jsx
@@ -1,9 +1,10 @@
-import React, { usestate } from "react";
+import React, { useState } from "react";
 
-export default function useState() {
-    const [arr1, setArr1] =useState([10,20,30,40,50]);
-    const[val1, setVal1] =usestate(0);
-    const[val2, setVal2] =usestate(0);
+// Array operations with useState demo
+export default function UseState3() {
+    const [arr1, setArr1] = useState([10, 20, 30, 40, 50]);
+    const [val1, setVal1] = useState(0);
+    const [val2, setVal2] = useState(0);
 
     const addElement = (v1) => {
         setArr1([...arr1, v1]);
@@ -12,26 +13,28 @@ export default function useState() {
     const removeElement =(v1) => {
         setArr1(arr1.filter((x) => x !== v1));
     };
-    const updateElement =(k1, v1) => {};
+    const updateElement = (oldValue, newValue) => {
+        setArr1(arr1.map((x) => (x === oldValue ? newValue : x)));
+    };
     return (
         <>
-        <h1> Array elements are: {arr1}</h1>
-        <br />
-        <input
-        type="number"
-        onChange={(e) => setVal1(parseInt(e.target.value))}
-        placeholder="Enter value to add"
-        />
-        <br />
-        <input 
-        type="number"
-        onChange={(e) => setVal2(parseInt(e.target.value))}
-        placeholder="Enter value to remove"
-        />
+            <h1>Array elements are: {arr1.join(", ")}</h1>
+            <br />
+            <input
+                type="number"
+                onChange={(e) => setVal1(parseInt(e.target.value))}
+                placeholder="Enter value"
+            />
+            <br />
+            <input
+                type="number"
+                onChange={(e) => setVal2(parseInt(e.target.value))}
+                placeholder="Enter new / remove value"
+            />
 
-        <button onClick={() => addElement(val1)}>Add</button>
-        <button onClick={() => removeElement(val2)}>remove</button>
-        <button onClick={() => updateElement(val1, val2)}>update</button>
+            <button onClick={() => addElement(val1)}>Add</button>
+            <button onClick={() => removeElement(val1)}>Remove</button>
+            <button onClick={() => updateElement(val1, val2)}>Update</button>
         </>
     );
 
